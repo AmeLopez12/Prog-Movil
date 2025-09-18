63903790d3dbc93e91488a9e52f1e79709916c52
AmeLopez12 <americalopezlemus@gmail.com>
2025-09-18 11:38:49 -0600

    Paso 7 Cómo recordar en la composición y traducciones

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -12,17 +12,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+//import androidx.compose.foundation.layout.Row
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-
         if (count > 0) {
-            Text("You've had $count glasses.")
+            Text("Has tomado $count vasos.")
         }
         Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
-            Text("Add one")
+            Text("Agrega uno")
         }
     }
 }
